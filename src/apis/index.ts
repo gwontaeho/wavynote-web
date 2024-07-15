a2f495af6c79ee4b7eb8f5b2555a688428bf9cf8
gwontaeho <75310299+gwontaeho@users.noreply.github.com>
2024-07-15 10:29:27 +0900

    feat: rest api

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,5 +1,106 @@
 import axios from "axios";
 
+// const api = axios.create({ baseURL: "https://abc-wavynote.koyeb.app/wavynote/v1.0" });
 const api = axios.create({ baseURL: "http://localhost:3001" });
+
+/*******************************************************/
+
+/**
+ *
+ */
+const signin = () => {};
+
+/**
+ *
+ */
+const signup = () => {};
+
+/**
+ *
+ */
+const duplicate = () => {};
+
+/*******************************************************/
+
+/**
+ * GET
+ * @description Get conversations
+ */
+const getConversations = () => {};
+
+/**
+ * GET
+ * @description Get box list
+ */
+const getBoxes = () => {};
+
+/**
+ * GET
+ * @description Get box detail
+ */
+const getBox = () => {};
+
+/*******************************************************/
+
+/**
+ * @description Get open-note list
+ */
+const getOpennotes = () => {};
+
+/**
+ * @description Get open-note detail
+ */
+const getOpennote = () => {};
+
+/**
+ * @description Get open-note searches
+ */
+const getOpennoteSearches = () => {};
+
+/*******************************************************/
+
+/**
+ * GET
+ * @description Get folder list
+ */
+const getFolders = () => {};
+
+/**
+ * GET
+ * @description Get folder
+ */
+const getFolder = () => {};
+
+/**
+ * GET
+ * @description Get folder searches
+ */
+const getFolderSearches = () => {};
+
+/**
+ * GET
+ * @description Get folders searches
+ */
+const getFoldersSearches = () => {};
+
+/**
+ * POST
+ * @description Create folder
+ */
+const createFolder = () => {};
+
+/**
+ * DELETE
+ * @description Delete folder
+ */
+const deleteFolder = () => {};
+
+/**
+ * PUT
+ * @description Update folder
+ */
+const updateFolder = () => {};
+
+/*******************************************************/
 
 export default api;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,7 @@
 export const Button = ({ children, ...props }: any) => {
-  return (
-    <button
-      {...props}
-      className="border h-[50px] rounded-[15px] bg-black text-white"
-    >
-      {children}
-    </button>
-  );
+    return (
+        <button {...props} className="border h-[50px] rounded-[15px] bg-black text-white">
+            {children}
+        </button>
+    );
 };
