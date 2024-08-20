608f2ee32e2bda7bfe3492683e61bfdadbaefc21
jinaoh <jina7430@gmail.com>
2024-08-20 18:31:16 +0900

    design: 폴더 목록 마크업 & 스타일

--- src/app/(main)/_components/Header.tsx
+++ src/app/(main)/_components/Header.tsx
@@ -1,3 +1,18 @@
-export const Header = () => {
-    return <header className="h-10"></header>;
+import { IconBtn } from "@/components/IconBtn";
+
+interface HeaderProps {
+    title: string;
+    buttonText: string;
+  }
+
+export const Header = ({ title, buttonText } : HeaderProps ) => {
+    return <header className="flex justify-between items-center h-[60px] px-[20px]">
+        <div className="titleWrap">
+            <h2>{title}</h2>
+        </div>
+        <div className="headerBtnWrap">
+            <button className="textBtn default">{buttonText}</button>
+            <IconBtn name="" type="search"></IconBtn>
+        </div>
+    </header>;
 };
